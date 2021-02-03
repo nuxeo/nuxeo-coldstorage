59dc38ab5ca642a88276092873437613ced68d4a
Abdoul BA <aba@nuxeo.com>
2021-02-03 09:20:53 +0100

    NXP-30107: update version format in case of Pull request

--- ci/jenkinsfiles/common-lib.groovy
+++ ci/jenkinsfiles/common-lib.groovy
@@ -191,7 +191,7 @@ String getReleaseVersion() {
 }
 
 String getPullRequestVersion() {
-  return "${readMavenPom().getVersion()}-${BRANCH_NAME}"
+  return readMavenPom().getVersion().replace('SNAPSHOT', "${BRANCH_NAME}-SNAPSHOT")
 }
 
 void gitCheckout(String reference) {
