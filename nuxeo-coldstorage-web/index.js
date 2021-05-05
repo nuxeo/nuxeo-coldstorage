6592181f1e11c807b460549193dfa889a0c84d4e
Andrea Cornaglia <acornaglia@nuxeo.com>
2021-05-05 10:30:35 +0200

    NXP-30381: restore from coldstorage front end

--- nuxeo-coldstorage-web/index.js
+++ nuxeo-coldstorage-web/index.js
@@ -19,5 +19,6 @@ import './elements/nuxeo-move-contents-to-coldstorage-button.js';
 import './elements/nuxeo-retrieve-content-from-coldstorage-button.js';
 import './elements/icons/coldstorage-icons.js';
 import './elements/nuxeo-coldstorage-infobar.js';
+import './elements/nuxeo-restore-content-from-coldstorage-button.js';
 
 import './nuxeo-coldstorage.html';
