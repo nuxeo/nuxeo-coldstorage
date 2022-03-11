70d032c8dd32845d19c0b837c74bbdefac7af77a
Guillaume Renard <grenard@nuxeo.com>
2022-03-11 17:29:12 +0100

    NXP-30931: to be reverted when NXP-30931 done

--- nuxeo-coldstorage/src/test/java/org/nuxeo/coldstorage/service/AbstractTestColdStorageService.java
+++ nuxeo-coldstorage/src/test/java/org/nuxeo/coldstorage/service/AbstractTestColdStorageService.java
@@ -379,6 +379,8 @@ public abstract class AbstractTestColdStorageService {
     public void shouldNotRecomputeFullText() {
         StorageConfiguration storageConfiguration = coreFeature.getStorageConfiguration();
         assumeTrue("fulltext search not supported", storageConfiguration.supportsFulltextSearch());
+        // XXX following line to be removed when https://jira.nuxeo.com/browse/NXP-30931 done
+        assumeTrue("Waiting for NXP-30931", !coreFeature.getStorageConfiguration().isVCS());
 
         // Create a doc with 'foo' text as main content
         DocumentModel documentModel = createFileDocument(DEFAULT_DOC_NAME, true);
