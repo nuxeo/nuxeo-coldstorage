406c77aa625ff8b206113e023a4ee6a2d2861a74
Guillaume Renard <grenard@nuxeo.com>
2022-04-05 13:20:07 +0200

    NXP-30948: introduce move to cold storage bulk action

---
+++ nuxeo-coldstorage/src/main/java/org/nuxeo/coldstorage/action/MoveToColdStorageContentAction.java
@@ -0,0 +1,85 @@
+/*
+ * (C) Copyright 2022 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume Renard<grenard@nuxeo.com>
+ */
+
+package org.nuxeo.coldstorage.action;
+
+import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.STATUS_STREAM;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.INPUT_1;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.OUTPUT_1;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.coldstorage.service.ColdStorageService;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.ecm.core.bulk.action.computation.AbstractBulkComputation;
+import org.nuxeo.lib.stream.computation.Topology;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.stream.StreamProcessorTopology;
+
+/**
+ * Bulk move documents to Cold Storage.
+ *
+ */
+public class MoveToColdStorageContentAction implements StreamProcessorTopology {
+
+    private static final Logger log = LogManager.getLogger(MoveToColdStorageContentAction.class);
+
+    public static final String ACTION_NAME = "moveToColdStorage";
+
+    @Override
+    public Topology getTopology(Map<String, String> options) {
+        return Topology.builder()
+                       .addComputation(MoveToColdStorageContentComputation::new, //
+                               List.of(INPUT_1 + ":" + ACTION_NAME, OUTPUT_1 + ":" + STATUS_STREAM))
+                       .build();
+    }
+
+    public static class MoveToColdStorageContentComputation extends AbstractBulkComputation {
+
+        public MoveToColdStorageContentComputation() {
+            super(ACTION_NAME);
+        }
+
+        @Override
+        protected void compute(CoreSession session, List<String> ids, Map<String, Serializable> properties) {
+            log.debug("Start computing documents to be sent to ColdStorage {}", ids);
+            DocumentModelList documents = loadDocuments(session, ids);
+
+            ColdStorageService service = Framework.getService(ColdStorageService.class);
+
+            for (DocumentModel document : documents) {
+                try {
+                    service.moveToColdStorage(session, document.getRef());
+                } catch(NuxeoException e) {
+                    log.warn("Document cannot be moved to Cold Storage: {}",e::getMessage);
+                    continue;
+                }
+            }
+            log.debug("End computing documents to be sent to ColdStorage");
+        }
+    }
+
+}
--- nuxeo-coldstorage/src/test/java/org/nuxeo/coldstorage/service/TestDummyColdStorageService.java
+++ nuxeo-coldstorage/src/test/java/org/nuxeo/coldstorage/service/TestDummyColdStorageService.java
@@ -20,6 +20,7 @@
 package org.nuxeo.coldstorage.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.nuxeo.ecm.core.api.security.SecurityConstants.READ;
 import static org.nuxeo.ecm.core.api.security.SecurityConstants.WRITE;
@@ -43,6 +44,7 @@ import javax.inject.Inject;
 import org.junit.Test;
 import org.nuxeo.coldstorage.ColdStorageConstants;
 import org.nuxeo.coldstorage.DummyColdStorageFeature;
+import org.nuxeo.coldstorage.action.MoveToColdStorageContentAction;
 import org.nuxeo.coldstorage.action.PropagateMoveToColdStorageContentAction;
 import org.nuxeo.ecm.core.DummyBlobProvider;
 import org.nuxeo.ecm.core.api.Blobs;
@@ -51,13 +53,18 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.api.event.CoreEventConstants;
+import org.nuxeo.ecm.core.api.security.SecurityConstants;
 import org.nuxeo.ecm.core.blob.BlobStatus;
 import org.nuxeo.ecm.core.blob.ManagedBlob;
+import org.nuxeo.ecm.core.bulk.BulkService;
+import org.nuxeo.ecm.core.bulk.message.BulkCommand;
+import org.nuxeo.ecm.core.bulk.message.BulkStatus;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventService;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.core.event.test.CapturingEventListener;
 import org.nuxeo.ecm.core.io.download.DownloadService;
+import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.LogCaptureFeature;
@@ -74,6 +81,28 @@ public class TestDummyColdStorageService extends AbstractTestColdStorageService 
     @Override
     protected String getBlobProviderName() {
         return "dummy";
+    }
+
+    @Test
+    public void shouldBulkMoveToColdStorage() throws IOException {
+        List<DocumentModel> docs = new ArrayList<DocumentModel>();
+        for (int i = 0; i < 10; i++) {
+            docs.add(createFileDocument(DEFAULT_DOC_NAME, Blobs.createBlob(FILE_CONTENT + i)));
+        }
+        transactionalFeature.nextTransaction();
+        String query = "SELECT * FROM File";
+
+        BulkService bulkService = Framework.getService(BulkService.class);
+        String username = SecurityConstants.SYSTEM_USERNAME;
+        String commandId = bulkService.submit(
+                new BulkCommand.Builder(MoveToColdStorageContentAction.ACTION_NAME, query, username).build());
+        coreFeature.waitForAsyncCompletion();
+
+        BulkStatus status = bulkService.getStatus(commandId);
+        assertNotNull(status);
+        for (DocumentModel doc : docs) {
+            verifyContent(session, doc.getRef(), true, FILE_CONTENT);
+        }
     }
 
     @Test
