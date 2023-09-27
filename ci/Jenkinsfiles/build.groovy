06fc979d894fd01cf51a91ffa65c5891530795ae
Guillaume Renard <guillaume.renard@hyland.com>
2023-09-27 11:11:46 +0200

    NXP-32093: Tmp disable ftests waiting for web-ui ftest framework nodejs 18 upgrade

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -163,6 +163,8 @@ pipeline {
             }
           }
           nxWithGitHubStatus(context: 'ftests') {
+            echo "Functional tests are disabled (https://jira.nuxeo.com/browse/WEBUI-1260 and https://jira.nuxeo.com/browse/WEBUI-1252)"
+            /*
             script {
               def testNamespace = "${CURRENT_NAMESPACE}-coldstorage-${BRANCH_NAME}-${BUILD_NUMBER}-ftests".replaceAll('\\.', '-').toLowerCase()
               def nuxeoParentVersion = readMavenPom().getParent().getVersion()
@@ -177,6 +179,7 @@ pipeline {
                 }
               }
             }
+            */
           }
         }
       }
