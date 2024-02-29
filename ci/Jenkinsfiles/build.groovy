1b845ee01639068eaf5c75f74c026735e87ae050
rahuljain-dev <rahul.jain@contractors.onbase.com>
2024-02-29 10:54:29 +0530

    test coldstorage checks

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -13,6 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *
+* test checks
 * Contributors:
 *     Kevin Leturc <kevin.leturc@hyland.com>
 */
