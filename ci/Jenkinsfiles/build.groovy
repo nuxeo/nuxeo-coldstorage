e1e2d73494ed5b28f47d80c097c6b95a711a4890
rahuljain-dev <rahul.jain@contractors.onbase.com>
2024-02-29 13:13:15 +0530

    installing test runner

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -121,6 +121,7 @@ pipeline {
                 dir('nuxeo-coldstorage-web') {
                   sh 'npm install --no-save playwright'
                   sh 'npx playwright install --with-deps'
+                  sh 'npm install @web/test-runner'
                   sh 'web-test-runner --version'
                   sh 'npm run test'
                 }
