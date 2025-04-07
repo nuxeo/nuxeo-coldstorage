0d14ef169e1b10e3522925d5659ca8ab81dae42a
Kevin Leturc <kevin.leturc@hyland.com>
2025-04-07 11:17:32 +0200

    NXP-32428: Update shared library

--- ci/Jenkinsfiles/release.groovy
+++ ci/Jenkinsfiles/release.groovy
@@ -19,7 +19,7 @@
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-library identifier: "platform-ci-shared-library@v0.0.45"
+library identifier: "platform-ci-shared-library@v0.0.53"
 
 pipeline {
   agent {
